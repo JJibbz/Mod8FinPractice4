4a7252be97c81c7a06951b07c9d7bed6f5177f91
JJibbz <vkazancev322@gmail.com>
2024-01-24 23:55:46 +0700

    Добавьте файлы проекта.

---
+++ Mod8FinPractice4/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod8FinPractice4
+{
+    internal class Student
+    {
+        internal string Name { get; set; }
+        internal string Group { get; set; }
+        internal DateTime DateOfBirth { get; set; }
+        internal decimal AvgPoint { get; set; }
+    }
+}
